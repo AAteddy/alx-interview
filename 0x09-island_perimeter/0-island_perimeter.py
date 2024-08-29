3bd247b1bbf21342601512521ef32d9e817f5c5e
Tedros Alem <tedsaasfaha@gmail.com>
2024-08-29 22:09:42 +0300

    updated

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,7 +8,8 @@ def island_perimeter(grid):
     Returns the perimeter of the island described in the grid.
 
     Parameters:
-    grid (list of list of int): A 2D grid representing the map, where 0 is water and 1 is land.
+    grid (list of list of int): A 2D grid representing the map, 
+    where 0 is water and 1 is land.
 
     Returns:
     int: The perimeter of the island.
